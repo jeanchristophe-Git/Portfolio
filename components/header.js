5fd3a3b76ef692a01e4ba7b92823e5b4db047f8e
jeanchristophe-Git <156956277+jeanchristophe-Git@users.noreply.github.com>
2024-05-18 18:58:18 +0000

    add certification badge

--- components/header.js
+++ components/header.js
@@ -3,13 +3,18 @@ import NavSection from "./nav";
 import ProjectSection from "./project";
 import TechnoSection from "./techno";
 
+//icons
+import { BadgeCheck } from "lucide-react";
 
 export default function HeaderSection() {
   return (
     <div>
       <NavSection/>
-      <div className=" ml-5 md:ml-0 mt-32   " >
-        <h1 className=" font-medium text-slate-800 dark:text-slate-200  " >Jean Christophe Désiré Bogbé</h1>
+      <div className=" ml-5 md:ml-0 mt-32 w-full   " >
+        <div className="flex space-x-2 items-center " >
+        <h1 className="  font-medium text-slate-800 dark:text-slate-200  " >Jean Christophe Désiré Bogbé </h1>
+        <BadgeCheck  className=" w-5 text-blue-600  "  />
+        </div>
         <p className="text-gray-500" >Je conçois & construis des interfaces </p>
       </div>
       <ProjectSection />
